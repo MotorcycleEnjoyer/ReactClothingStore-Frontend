eefa5e4d550717493b09583a3b9af626ba89201d
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-01-23 14:05:10 -0500

    move register skeleton into component

---
+++ frontend/src/Components/Register/Register.js
@@ -0,0 +1,10 @@
+export default function Register(){
+    return(
+        <>
+            <h1>Register account!</h1>
+            <form>
+            </form>
+            <button onClick={()=>{window.location="/"}}>Home</button>
+        </>
+    )
+}
