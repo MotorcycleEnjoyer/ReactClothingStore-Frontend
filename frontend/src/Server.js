d650a81080162acde1efee1b98f00784b272b11c
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-01-23 04:19:46 -0500

    backend regex and search suggestions

--- frontend/src/Server.js
+++ frontend/src/Server.js
@@ -1,14 +1,34 @@
 const express = require("express")
 const app = express()
 const dummyProductDB = require("./DummyProductDB")
+const suggestionDB = require("./Suggestions")
 const cors = require("cors")
 app.use(cors())
+app.use(express.json())
+
+console.log(suggestionDB)
 
 const dummyData = dummyProductDB.userData
 
 app.get('/data', function(req,res){
     console.log("DATA REQUEST")
     res.send(dummyData)
+})
+
+app.post('/suggestions', function(req,res){
+    console.log("SUGGESTIONS REQUEST")
+    let phrase = req.body.searchTerm
+    let regex
+    try{
+        regex = new RegExp(phrase, 'gi');
+    }catch(e){
+        console.error(e)
+    }
+
+    let searchSuggestions = suggestionDB.filter(item => {
+        return item.match(regex)
+    })
+    res.send(searchSuggestions)
 })
 
 app.listen(5000, console.log("Running on port 5000"))
