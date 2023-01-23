63f40fa4de44ee1f869d8a1fa56f5bc131b91eec
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-01-23 04:19:03 -0500

    backend regex and search suggestions

--- frontend/src/Components/Search/Search.js
+++ frontend/src/Components/Search/Search.js
@@ -1,11 +1,26 @@
 import React from "react"
 import ShoppingProduct from "../ShoppingProduct/ShoppingProduct"
+import axios from "axios"
 
 export default function Search({...props}){
     const [searchVal, setSearchVal] = React.useState("")
     const [searchResults, setSearchResults] = React.useState([])
     const [submittedResults, setSubmittedResults] = React.useState([])
     const [blockedCharacters, setBlockedCharacters] = React.useState(new RegExp("[~`!@#$%^&()_={}\\[\\]\\:;,\\.\\/<>\\\\*\\-+\\?]"))
+    const SUGGESTIONS_URL = "http://localhost:5000/suggestions"
+      
+        function getSuggestions(value){
+        const data = {
+            searchTerm: value
+        }
+        axios.post(SUGGESTIONS_URL, data).then(function(response){
+          setSearchResults(response.data || [])
+        }).catch(error=>{
+          if(error.response){
+            console.log(error.response.data, error.response.status, error.response.headers)
+          }
+        })
+      }
 
     function checkEnter(e){
         if(e.key === "Enter")
@@ -35,45 +50,30 @@ export default function Search({...props}){
         changeSearchValueIfProperRegex(e.target.value)
     }
 
-    function findItemInDummyDB(phrase){
-        let regex
-        try{
-            regex = new RegExp(phrase, 'gi');
-        }catch(e){
-            console.error(e)
-        }
-        
-        setSearchResults(
-            props.userData.cart.filter(item => {
-                return item.name.match(regex)
-            })
-        )
-    }
-
     React.useEffect(()=>{
         if(searchVal === ""){
             setSearchResults([])
         }else{
-            findItemInDummyDB(searchVal)
+            getSuggestions(searchVal)
         }
             
     }, [searchVal])
 
     React.useEffect(()=>{
-        props.storeSearchResults(searchResults)
+        // props.storeSearchResults(searchResults)
     },[submittedResults])
 
     React.useEffect(()=>{
         console.log("SEARCH RESULTS")
         console.log(searchResults)
         console.log("SEARCH RESULTS END")
-        if(!props.modalStatus){
+        /* if(!props.modalStatus){
             props.storeSearchResults(searchResults)
-        }
+        } */
         // props.storeSearchResults(searchResults)
     },[searchResults])
 
-    const resultsAsHTML = searchResults.map((x, index) => <ShoppingProduct key={index} {...x} storeSearchValFromClick={storeSearchValFromClick} view="searchDropDown"/>)
+    const resultsAsHTML = searchResults.map((x, index) => <ShoppingProduct key={index} name={x} storeSearchValFromClick={storeSearchValFromClick} view="searchDropDown"/>) 
 
     return(
         <div className="search">
