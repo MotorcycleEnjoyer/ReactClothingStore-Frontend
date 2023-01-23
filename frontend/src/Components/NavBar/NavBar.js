c28259ca8625287e68b3d0b088ed133468bb25e3
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-01-23 14:05:00 -0500

    update navbar to redirect instead of change view

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -2,13 +2,12 @@ import Search from "../Search/Search"
 import React from "react"
 
 export default function NavBar({...props}){
-
     return(
         <nav className="navBar">
             <Search modalStatus={props.modalStatus} userData={props.userData} showModal={props.showModal} hideModal={props.hideModal} storeSearchResults={props.storeSearchResults} selectProduct={props.selectProduct}/>
-            <button onClick={props.changeView}>LOGIN</button>
-            <button onClick={props.changeView}>REGISTER</button>
-            <button onClick={props.changeView}>MY CART</button>
+            <button onClick={()=> window.location.assign("/login")}>LOGIN</button>
+            <button onClick={()=> window.location.assign("/register")}>REGISTER</button>
+            <button onClick={()=> window.location.assign("/cart")}>MY CART</button>
         </nav>
     )
 }
