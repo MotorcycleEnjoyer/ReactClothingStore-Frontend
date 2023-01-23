2910d9a047fa5368fe04f2d35e95a7cb5e5b7c68
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-01-23 14:03:51 -0500

    make app multi page with BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,159 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import './Components/ShoppingProduct/ShoppingProduct.css'
-import './Components/Search/Search.css'
-import './Components/NavBar/NavBar.css'
-import NavBar from "./Components/NavBar/NavBar"
 import React from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import Homepage from './Components/Homepage/Homepage';
+import Login from './Components/Login/Login';
 import ShoppingProduct from './Components/ShoppingProduct/ShoppingProduct';
-import axios from 'axios'
+import './App.css'
+import './Components/Search/Search.css'
+import './Components/ShoppingProduct/ShoppingProduct.css';
+import './Components/NavBar/NavBar.css'
+import Register from './Components/Register/Register';
+import Cart from './Components/Cart/Cart';
 
 const DATA_URL = "http://localhost:5000/data"
 
 function App() {
-
-  const [userData, setUserData] = React.useState({
-    session: "9a2a94eb-c327-4625-bd4d-cb9e1d53bcfb",
-    cart: [
-      {
-        id: 1,
-        name: "Generic T Shirt",
-        manufacturerOrBrand: "T-Shirt-CO",
-        size: 5,
-        ageCategory: "5-10",
-        sexCategory: "M",
-        typeOfClothing: "T-Shirt",
-        colorOptions: ["red", "green", "orange", "pink"],
-        imagePreviewURL: "a",
-        variationIsInStock: [{color: "red", amountInStock: 10}, {color: "green", amountInStock: 3}, {color: "orange", amountInStock: 0}, {color:"pink", amountInStock:2}],
-        weight: {grams: 50},
-        dimensions: "LxWxH",
-        price: 5.99,
-        materials: {polyester: "50%", cotton: "50%"},
-        amount: 1,
-        totalCost: 5.99,
-    },
-    {
-        id: 2,
-        name: "Not Generic T Shirt",
-        manufacturerOrBrand: "T-Shirt-CO",
-        size: 5,
-        ageCategory: "10-15",
-        sexCategory: "F",
-        typeOfClothing: "T-Shirt",
-        colorOptions: ["red", "green", "orange", "pink"],
-        imagePreviewURL: "a",
-        variationIsInStock: [{color: "red", amountInStock: 2}, {color: "green", amountInStock: 3}, {color: "orange", amountInStock: 4}, {color:"pink", amountInStock: 5}],
-        weight: {grams: 49},
-        dimensions: "LxWxH",
-        price: 5.99,
-        materials: {polyester: "50%", cotton: "50%"},
-        amount: 1,
-        totalCost: 5.99,
-        }
-        ],
-})
-  const [currentView, setCurrentView] = React.useState("SEARCH")
-  const [selectedProduct, setSelectedProduct] = React.useState("NONE")
-  const [searchResults, setSearchResults] = React.useState([])
-  const [modalStatus, setModalStatus] = React.useState(false)
-
-  function hideModal(){
-    document.querySelector(".search--modal").style.display = "none"
-    setModalStatus(false)
-  }
-
-  function showModal(){
-    document.querySelector(".search--modal").style.display = "block"
-    setModalStatus(true)
-  }
-
-  function storeSearchResults(results){
-    console.log(results)
-    setSearchResults(results)
-    // setCurrentView("SEARCH")
-  }
-
-  React.useEffect(()=>{
-    setCurrentView("SEARCH")
-  },[searchResults])
-
-  const dataAsCartView = searchResults.map((item, index) => <ShoppingProduct key={index} selectProduct={selectProduct} {...item} view="searchResult"/>)
-
-  function selectProduct(id){
-    let data = userData.cart.filter(item => item.id === id)[0]
-    if(data===undefined)
-      return
-    hideModal()
-    setSelectedProduct(data)
-    console.log(selectedProduct)
-    setCurrentView("PRODUCT")
-  }
-  
-  function changeView(e){
-      console.log(e.target.innerText)
-      setCurrentView(e.target.innerText)
-  }
-
-  function toggleModal(e){
-    let target = e.target
-    let classType = target.className
-    if(classType ==="search--modal--clickListener" || classType==="navBar" || target.type ==="submit"){
-      hideModal()
-    }
-    if(classType === "search--inputBox"){
-      showModal()
-    }
-  }
-
   return (
-    <div className="App" onClick={toggleModal}>
-      <NavBar modalStatus={modalStatus} userData={userData} showModal={showModal} hideModal={hideModal} storeSearchResults={storeSearchResults} selectProduct={selectProduct} changeView={changeView}/>
-      <div className='mainContaier'>
-      { currentView === "SEARCH" &&
-      <>
-          <div className="mainContainer--sideBar">SIDEBAR</div>
-          <div className="mainContainer--results">
-            {dataAsCartView}
-          </div>
-      </>
-        
-      }
-      {
-        currentView === "LOGIN" &&
-        <>
-          <h1>Please enter your credentials</h1>
-          <input type="text" name="username" placeholder='Username'></input>
-          <input type="password" name="password" placeholder='Password'></input>
-          <button onClick={changeView}>SEARCH</button>        
-        </>
-        }
-        {
-          currentView === "REGISTER" &&
-          <>
-            <h1>Register account!</h1>
-            <form>
-            </form>
-            <button onClick={changeView}>SEARCH</button>
-          </>
-        }
-        {
-          currentView === "MY CART" &&
-          <>
-            <button onClick={changeView}>SEARCH</button>
-            <h1>My Shopping Cart</h1>
-            <div>
-              {dataAsCartView}
-            </div>
-          </>
-        }
-        {
-          currentView === "PRODUCT" &&
-          <>
-              <ShoppingProduct {...selectedProduct} view="fullSize"/>
-          </>
-        }
-        </div>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Homepage />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/register" element={<Register />}/>
+            <Route path="/cart" element={<Cart />}/>
+          </Routes>
+        </BrowserRouter>
       </div>
   );
 }
