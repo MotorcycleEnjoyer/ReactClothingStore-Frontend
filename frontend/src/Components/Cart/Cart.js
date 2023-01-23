5aa27b39aec52e6b37f56d28309e9d7781888cdf
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-01-23 14:04:16 -0500

    put cart skeleton into component

---
+++ frontend/src/Components/Cart/Cart.js
@@ -0,0 +1,10 @@
+export default function Cart(){
+    return(
+        <>
+            <button onClick={()=>window.location="/"}>HOME</button>
+            <h1>My Shopping Cart</h1>
+            <div>
+            </div>
+        </>
+    )
+}
