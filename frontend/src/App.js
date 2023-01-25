3d7417cf10c4566d4fb0d94f91b2bc4b5da8f962
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-01-25 16:23:54 -0500

    make a dedicated searchResult page and 404

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import './Components/ShoppingProduct/ShoppingProduct.css';
 import './Components/NavBar/NavBar.css'
 import Register from './Components/Register/Register';
 import Cart from './Components/Cart/Cart';
+import NoMatch from './Components/NoMatch/NoMatch';
 
 const DATA_URL = "http://localhost:5000/data"
 
@@ -23,6 +24,8 @@ function App() {
             <Route path="/login" element={<Login />}/>
             <Route path="/register" element={<Register />}/>
             <Route path="/cart" element={<Cart />}/>
+            <Route path="/s" element={<Homepage searchIsDone={true}/>}/>
+            <Route path="/*" element={<NoMatch />} />
           </Routes>
         </BrowserRouter>
       </div>
