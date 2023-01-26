d122e96a7520681c3e42d634b82d653c5d7186ee
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-01-26 12:49:07 -0500

    add home button
    also removed a useless prop storeSearchResults.

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -4,7 +4,8 @@ import React from "react"
 export default function NavBar({...props}){
     return(
         <nav className="navBar">
-            <Search modalStatus={props.modalStatus} userData={props.userData} showModal={props.showModal} hideModal={props.hideModal} storeSearchResults={props.storeSearchResults} selectProduct={props.selectProduct}/>
+            <button onClick={()=>window.location.assign("/")}>HOME</button>
+            <Search modalStatus={props.modalStatus} userData={props.userData} showModal={props.showModal} hideModal={props.hideModal} selectProduct={props.selectProduct}/>
             <button onClick={()=> window.location.assign("/login")}>LOGIN</button>
             <button onClick={()=> window.location.assign("/register")}>REGISTER</button>
             <button onClick={()=> window.location.assign("/cart")}>MY CART</button>
