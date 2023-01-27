4c575380ac1ddb43e3b41eabd111582a9171075f
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-01-27 12:04:47 -0500

    remove unused prop

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar({...props}){
     return(
         <nav className="navBar">
             <button onClick={()=>window.location.assign("/")}>HOME</button>
-            <Search modalStatus={props.modalStatus} userData={props.userData} showModal={props.showModal} hideModal={props.hideModal} selectProduct={props.selectProduct}/>
+            <Search modalStatus={props.modalStatus} userData={props.userData} showModal={props.showModal} hideModal={props.hideModal}/>
             <button onClick={()=> window.location.assign("/login")}>LOGIN</button>
             <button onClick={()=> window.location.assign("/register")}>REGISTER</button>
             <button onClick={()=> window.location.assign("/cart")}>MY CART</button>
