43bd7f8f1cf99eac129a61b2cfafc415bda246b7
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-01-31 10:50:26 -0500

    create login function
    added routes, post req, and server response div

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -1,15 +1,37 @@
-export default function Login(){
-    function handleSubmit(){
+import axios from "axios"
 
+const LOGIN_ENDPOINT = "http://localhost:5000/login"
+const FAILURE_STRING = "Incorrect credentials. Please try again"
+const HOMEPAGE = "/"
+
+export default function Login(){
+    function handleSubmit(e){
+        e.preventDefault()
+        let credentials = {}
+        credentials["username"] = document.querySelector("#username").value
+        credentials["password"] = document.querySelector("#password").value
+        axios.post(LOGIN_ENDPOINT, credentials, {withCredentials: true})
+            .then((response)=>
+            {
+                console.log(response)
+                if(response.data === FAILURE_STRING){
+                    document.querySelector(".serverResponse").innerText = FAILURE_STRING
+                }
+                else{
+                    window.location = HOMEPAGE
+                }
+            })
+            .catch((error)=> console.log(error))
     }
     return(
         <>
             <h1>Please enter your credentials</h1>
             <form>
-                <input type="text" name="username" placeholder='Username' required></input>
-                <input type="password" name="password" placeholder='Password' required></input>
-                <button onClick={handleSubmit}></button>
+                <input type="text" id="username" name="username" placeholder='Username' required></input>
+                <input type="password" id="password" name="password" placeholder='Password' required></input>
+                <button onClick={handleSubmit}>Submit</button>
             </form>
+            <div className="serverResponse"></div>
         </>
     )
 }
