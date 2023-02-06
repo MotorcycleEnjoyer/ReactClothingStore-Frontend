55d501e6e40c02aba430405edf4eeb7d18b53b18
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-02-06 15:34:19 -0500

    rearrange imports for neatness

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
+
 import Homepage from './Components/Homepage/Homepage';
 import Homepage__PRODUCT from './Components/Homepage/Homepage__PRODUCT';
 import Homepage__SEARCH from './Components/Homepage/Homepage__SEARCH';
 import Login from './Components/Login/Login';
-import './App.css'
-import './Components/SearchBar/Search.css'
-import './Components/ShoppingProduct/ShoppingProduct.css';
-import './Components/NavBar/NavBar.css'
 import Register from './Components/Register/Register';
 import Cart from './Components/Cart/Cart';
 import FileNotFound from './Components/FileNotFound/FileNotFound';
 
-const DATA_URL = "http://localhost:5000/data"
+import './App.css'
+import './Components/SearchBar/Search.css'
+import './Components/ShoppingProduct/ShoppingProduct.css';
+import './Components/NavBar/NavBar.css'
 
 function App() {
   return (
