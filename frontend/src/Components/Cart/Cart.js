57995d42e950bdc71a1894834a061a99737872b9
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-02-08 02:39:35 -0500

    trying homepage view for cart
    I plan an Onclick that brings a product modal, where edits to details can be made.
    Also for quick edit without, QUANTITY, and DELETE buttons should be added to the ShoppingProduct cart view

--- frontend/src/Components/Cart/Cart.js
+++ frontend/src/Components/Cart/Cart.js
@@ -1,37 +1,66 @@
 import axios from "axios"
 import React from "react"
 import ShoppingProduct from "../ShoppingProduct/ShoppingProduct"
+import NavBar from "../NavBar/NavBar"
 
-export default function Cart(){
+export default function CartItems(){
+    const LOGOUT_URL = "http://localhost:5000/logout"
+    const [cartItems, setCartItems] = React.useState([])
+    const [modalStatus, setModalStatus] = React.useState(false)      
 
-    const [userData, setUserData] = React.useState([])
-
-    function getUserCartFromServer(){
-        axios.get("http://localhost:5000/shoppingCart", {withCredentials: true}).then((response) => {
-          setUserData(response.data.shoppingCart)
-          console.log(response.data)
-        }).catch(error => {
-          console.error(error)
-        })
-      }
+    function logout(){
+      axios.post(LOGOUT_URL, {dummy: 2}, {withCredentials: true})
+      .then(response => {
+        alert(response.data)
+        if(response.data === "Logged out successfully!")
+        {
+          window.location = "/"
+        }
+      })
+      .catch(error => console.error(error))
+  }
 
     React.useEffect(()=>{
-        getUserCartFromServer()
-    }, [])
+      getUsercartItemsFromServer()
+    },[])
 
-    function handleClick(){
-        // pull quantity values, if they changed at all, and send em to server.
+    function getUsercartItemsFromServer(){
+      axios.get("http://localhost:5000/shoppingCart", {withCredentials: true}).then((response) => {
+        let itemsInCart = response.data.shoppingCart.map((item, index) => <ShoppingProduct key={index} {...item} view={"cart"} />)
+        setCartItems(itemsInCart)
+      }).catch(error => {
+        console.error(error)
+      })
     }
 
-    const allCartItems = userData.map((item, index) => <ShoppingProduct key={index} {...item} view={"cart"} />)
+    function hideModal(){
+      document.querySelector(".search--modal").style.display = "none"
+      setModalStatus(false)
+    }
+  
+    function showModal(){
+      document.querySelector(".search--modal").style.display = "block"
+      setModalStatus(true)
+    }
+  
+    function toggleModal(e){
+      let target = e.target
+      let classType = target.className
+      if(classType ==="search--modal--clickListener" || classType==="navBar" || target.type ==="submit"){
+        hideModal()
+      }
+      if(classType === "search--inputBox"){
+        showModal()
+      }
+    }
+
     return(
-        <>
-            <button onClick={()=>window.location="/"}>HOME</button>
-            <h1>My Shopping Cart</h1>
-            <div>
-                {allCartItems}
-            </div>
-            <button onClick={handleClick}>Checkout</button>
-        </>
+      
+      <div onClick={toggleModal}>
+          <NavBar modalStatus={modalStatus} logout={logout} cartItems={cartItems} showModal={showModal} hideModal={hideModal}/>
+          <div className="cartItems">
+            {cartItems}
+          </div>
+      </div>
     )
 }
