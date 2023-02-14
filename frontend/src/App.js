acb16687dec62245e7931b053ba629932b03f453
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-02-14 17:07:31 -0500

    make APP source of truth
    instead of calling server for shopping cart on EVERY page I load, Call it in app and pass it via PROPS.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import axios from 'axios';
 
 import Homepage from './Components/Homepage/Homepage';
 import Homepage__PRODUCT from './Components/Homepage/Homepage__PRODUCT';
@@ -15,17 +16,62 @@ import './Components/ShoppingProduct/ShoppingProduct.css';
 import './Components/NavBar/NavBar.css'
 import './Components/CategoryButton/CategoryButton.css'
 
-function App() {
+export default function App() {
+  const BASE_URL = "http://localhost:5000"
+  const LOGOUT_URL = BASE_URL + "/logout"
+  const LOGIN_URL = BASE_URL + "login"
+  const DELETION_URL = BASE_URL + "/deleteCartItem"
+  const [userShoppingCart, setUserShoppingCart] = React.useState("")
+
+  function fetchUserShoppingCart(){
+    axios.get(BASE_URL + "/shoppingCart", {withCredentials: true})
+    .then(response => {
+      setUserShoppingCart(response.data.shoppingCart)
+    }).catch(error => console.error(error))
+  }
+
+  function logout(){
+    axios.post(LOGOUT_URL, {dummy: 2}, {withCredentials: true})
+    .then(response => {
+      alert(response.data)
+      if(response.data === "POST/logout: Logged out successfully!")
+      {
+        window.location = "/"
+      }
+    })
+    .catch(error => console.error(error))
+  }
+
+  function removeFromCart(index){
+    console.log(index)
+    axios.post(DELETION_URL, {indexOfCartItem: index}, {withCredentials: true})
+    .then(response => {
+        setUserShoppingCart(response.data.shoppingCart)
+    })
+    .catch(error => console.error(error))
+  }
+
+  React.useEffect(()=>{
+    fetchUserShoppingCart()
+  },[])
+
   return (
       <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Homepage />}/>
-            <Route path="/s" element={<Homepage__SEARCH searchIsDone={true}/>}/>
-            <Route path="/p/*" element={<Homepage__PRODUCT  productIsSelected={true}/>}/>
+            { 
+              // PREVENT RENDERING UNTIL MAIN APP RECEIVES DATA FROM SERVER!
+              userShoppingCart !== "" &&
+              <>
+                <Route path="/" element={<Homepage logout={logout} cart={userShoppingCart}/>}/>
+                <Route path="/s" element={<Homepage__SEARCH logout={logout} cart={userShoppingCart} searchIsDone={true}/>}/>
+                <Route path="/p/*" element={<Homepage__PRODUCT logout={logout} cart={userShoppingCart} productIsSelected={true}/>}/>
+                <Route path="/cart" element={<Cart logout={logout} removeFromCart={removeFromCart} cart={userShoppingCart}/>}/>
+              </>
+              
+            }
             <Route path="/login" element={<Login />}/>
             <Route path="/register" element={<Register />}/>
-            <Route path="/cart" element={<Cart />}/>
             <Route path="/*" element={<FileNotFound />} />
           </Routes>
         </BrowserRouter>
@@ -33,4 +79,4 @@ function App() {
   );
 }
 
-export default App;
+
