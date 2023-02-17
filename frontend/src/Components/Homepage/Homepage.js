dd825c4172c643954bcad010b021617b5e0d4700
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-02-17 12:30:01 -0500

    remove navBar and modal logic

--- frontend/src/Components/Homepage/Homepage.js
+++ frontend/src/Components/Homepage/Homepage.js
@@ -1,42 +1,15 @@
 import React from "react";
 import CategoryButton from "../CategoryButton/CategoryButton";
 import allCategories from "../CategoryButton/categories"
-import NavBar from "../NavBar/NavBar"
 
-export default function Homepage({...props}){
-    const [modalStatus, setModalStatus] = React.useState(false)      
+export default function Homepage(){
     const [categories, setCategories] = React.useState(allCategories.dummyDB)
-
-    function hideModal(){
-      document.querySelector(".search--modal").style.display = "none"
-      setModalStatus(false)
-    }
-  
-    function showModal(){
-      document.querySelector(".search--modal").style.display = "block"
-      setModalStatus(true)
-    }
-  
-    function toggleModal(e){
-      let target = e.target
-      let classType = target.className
-      if(classType ==="search--modal--clickListener" || classType==="navBar" || target.type ==="submit"){
-        hideModal()
-      }
-      if(classType === "search--inputBox"){
-        showModal()
-      }
-    }
 
     const categoryDataAsHtml = categories.map((item, index) => <CategoryButton key={index} name={item} />)
 
     return(
-      
-      <div onClick={toggleModal}>
-          <NavBar modalStatus={modalStatus} isLoggedIn={props.isLoggedIn} logout={props.logout} length={props.length} showModal={showModal} hideModal={hideModal}/>
-          <div className="categoryButtonParentContainer">
-            {categoryDataAsHtml}
-          </div>
-      </div>
+        <div className="categoryButtonParentContainer">
+          {categoryDataAsHtml}
+        </div>
     )
 }
