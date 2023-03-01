e7da3e1eaed98f04c1fe53b326cbfac597b1c193
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-03-01 16:56:55 -0500

    remove random stuff

--- frontend/src/Routes/NewProduct.jsx
+++ frontend/src/Routes/NewProduct.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Link, useLoaderData} from "react-router-dom"
 import { getProduct, addToCart } from "../API/apiCalls"
 import ShoppingProduct from '../Components/SmallComponents/ShoppingProduct/ShoppingProduct'
+
 
 export async function loader({params}){
     document.title = `React Clothing Store: ${params.productName}`
@@ -9,19 +10,13 @@ export async function loader({params}){
     return { searchResults }
 }
 
-/*
-        let fadeModalContent = document.querySelector(".fadeModal--content")
-        fadeModalContent.innerText = response.data
-*/
-
-export default function NewNav(){
+export default function NewProduct(){
     const { searchResults } = useLoaderData()
     const product = <ShoppingProduct {...searchResults} addToCart={addToCart} view="fullSize"/>
     
     console.log(searchResults)
     return(
         <>
-            <h1>Second Bruh</h1>
             {product ?? <h1>no product</h1>}
         </>
     )
