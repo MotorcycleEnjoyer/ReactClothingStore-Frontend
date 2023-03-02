8b6663486fabb74193f5fff292f622f27de65bf4
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-03-02 13:21:00 -0500

    rewrite login

---
+++ frontend/src/Routes/NewLogin.jsx
@@ -0,0 +1,25 @@
+import { login } from "../API/apiCalls"
+
+async function handleSubmit(e){
+    e.preventDefault()
+    const rawFormData = new FormData(e.target)
+    const credentials = Object.fromEntries(rawFormData)
+    const result = await login(credentials)
+    if(result?.status === 401){
+        document.querySelector(".serverResponse").innerText = result.data
+    }
+}
+
+export default function Login(){
+    return(
+        <>
+            <h1>Please enter your credentials</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" id="username" name="username" placeholder='Username' required></input>
+                <input type="password" id="password" name="password" placeholder='Password' required></input>
+                <button>Submit</button>
+            </form>
+            <div className="serverResponse"></div>
+        </>
+    )
+}
