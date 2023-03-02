3725da41e470ff5a5aaafe51a96636fa1a749b3a
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-03-02 13:31:05 -0500

    rewrite registration page

---
+++ frontend/src/Routes/NewRegister.jsx
@@ -0,0 +1,33 @@
+import axios from "axios"
+import React from "react"
+import { register } from "../API/apiCalls"
+
+
+
+const NAME_COLLISION = "POST/register: Name is taken!"
+
+function handleSubmit(e){
+    e.preventDefault()
+    const rawFormData = new FormData(e.target)
+    const credentials = Object.fromEntries(rawFormData)
+    if(credentials.password === credentials.confirmPassword){
+        register(credentials)
+    }else{
+        document.querySelector("#message").innerText = "Password and Confirm Password do not match."
+    }
+}
+
+export default function Register(){
+    return(
+        <>
+            <h1>Register account!</h1>
+            <form onSubmit={handleSubmit}>
+                <input id="username" name="username" placeholder="Username" required></input>
+                <input id="password" name="password" placeholder="Password" required></input>
+                <input id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" required></input>
+                <button>Submit</button>
+            </form>
+            <div id="message"></div>
+        </>
+    )
+}
