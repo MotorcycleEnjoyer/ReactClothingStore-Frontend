50d7e475e38b4dc9157b5e4c0af005bd4bac1998
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-03-07 15:07:30 -0500

    add pwLength check on frontend

--- src/Routes/NewRegister.jsx
+++ src/Routes/NewRegister.jsx
@@ -13,14 +13,28 @@ function handleSubmit (e) {
 }
 
 export default function Register () {
+    const [password, setPassword] = React.useState("")
+    const [confirmPassword, setConfirmPassword] = React.useState("")
+
+    function handleConfirmPwChange (e) {
+        setConfirmPassword(e.target.value)
+    }
+
+    function handlePwChange (e) {
+        setPassword(e.target.value)
+    }
+
     return (
         <>
             <h1>Register account!</h1>
             <form onSubmit={handleSubmit}>
                 <input id="username" name="username" placeholder="Username" required></input>
-                <input id="password" name="password" placeholder="Password" required></input>
-                <input id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" required></input>
-                <button>Submit</button>
+                <input id="password" onChange={handlePwChange} name="password" placeholder="Password" value={password} required></input>
+                <input id="confirmPassword" onChange={handleConfirmPwChange} name="confirmPassword" value={confirmPassword} placeholder="Confirm Password" required></input>
+                {
+                    (password === confirmPassword && confirmPassword.length >= 8) &&
+                    <button>Submit</button>
+                }
             </form>
             <div id="message"></div>
         </>
