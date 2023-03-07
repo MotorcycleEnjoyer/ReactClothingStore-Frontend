5400cd682fb38a4f8fac526551e5c75b418f1904
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-03-07 15:06:11 -0500

    create homepage element

---
+++ src/Routes/NewHomePage.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import upwardArrow from "../krita-vertical-arrow.png"
+
+export default function NewHomePage () {
+    return (
+        <div className="homepageArrowBackground" style={{ backgroundImage: `url('${upwardArrow}')` }}></div>
+    )
+}
