0fd950263a66ce68ef7b21149453d2a0ef9097e1
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-03-14 15:09:34 -0400

    add createReview button, add login conditional

--- src/Components/SmallComponents/ReviewBox/ReviewBox.js
+++ src/Components/SmallComponents/ReviewBox/ReviewBox.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { addProductReview } from "../../../API/apiCalls"
 
-export default function ReviewBox ({ productId, initialReviews }) {
+export default function ReviewBox ({ productId, initialReviews, loggedIn }) {
     const [reviewArray, setReviewArray] = React.useState(initialReviews || [])
     const [review, setReview] = React.useState("")
+    const [hasNotSubmitted, setHasNotSubmitted] = React.useState(false)
     const remainingChars = 200 - review.length
     const reviewsAsHTML = reviewArray.map((item, index) => <div className="review" key={index}>{item}</div>)
 
@@ -16,26 +17,50 @@ export default function ReviewBox ({ productId, initialReviews }) {
         e.preventDefault()
         const rawFormData = new FormData(e.target)
         const { productReview } = Object.fromEntries(rawFormData)
+        if (productReview === "") {
+            return
+        }
         // const result = await login(credentials)
         const { reviews } = await addProductReview(productId, productReview)
         setReviewArray(() => reviews)
+        setReview(() => "")
+        setHasNotSubmitted(() => false)
         console.log(reviews)
+    }
+
+    const remainingCharactersStyle = {
+        color: remainingChars >= 50 ? "palegreen" : "yellow",
+        fontWeight: "bold",
+        fontSize: "50px"
     }
 
     return (
         <div className="productReviewContainer">
-            <form onSubmit={handleReviewSubmission}>
-                <textarea
-                    className="productReviewInput"
-                    name="productReview"
-                    maxLength="200"
-                    onChange={handleChange}
-                    value={review}>
-                </textarea>
-                <div>Maximum 200 characters || ( {remainingChars} Remaining )</div>
-                <button type="submit">Submit Review</button>
-            </form>
-            {reviewsAsHTML}
+            { loggedIn &&
+            <>
+                { !hasNotSubmitted && <button onClick={() => { setHasNotSubmitted(() => true) }}>Write a Review</button> }
+
+                { hasNotSubmitted &&
+                    <form className="productReviewForm" onSubmit={handleReviewSubmission}>
+                        <textarea
+                            className="productReviewInput"
+                            name="productReview"
+                            maxLength="200"
+                            onChange={handleChange}
+                            value={review}>
+                        </textarea>
+                        <div><span style={remainingCharactersStyle}>{200 - remainingChars}</span>/200</div>
+                        <button onClick={() => { setReview(() => ""); setHasNotSubmitted(() => false) }}>Cancel</button>
+                        <button type="submit">Submit Review</button>
+                    </form>
+                }
+            </>
+
+            }
+            <h1>REVIEWS</h1>
+            <div className="reviewsAsHTML" >
+                {((reviewsAsHTML.length > 0) && reviewsAsHTML) || <h3 style={{ textAlign: "center", color: "darkgreen" }}>Be the first to review!</h3>}
+            </div>
         </div>
     )
 }
