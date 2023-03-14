f7cac2c080af3ab98c3baa089e66d66c6885dc46
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-03-14 12:27:59 -0400

    added ratings/reviews to loader
    Also loading Reviews beneath the product.

--- src/Routes/NewProduct.jsx
+++ src/Routes/NewProduct.jsx
@@ -1,21 +1,26 @@
 import React from "react"
 import { useLoaderData } from "react-router-dom"
-import { getProduct, addToCart } from "../API/apiCalls"
+import { getProduct, addToCart, getInitialRatingsAndReviews } from "../API/apiCalls"
+import { LoginContext } from "../Contexts/ShoppingContext"
 import ShoppingProduct from "../Components/SmallComponents/ShoppingProduct/ShoppingProduct"
+import ReviewBox from "../Components/SmallComponents/ReviewBox/ReviewBox"
 
 export async function loader ({ params }) {
     document.title = `React Clothing Store: ${params.productName}`
     const searchResults = await getProduct(params.productId)
-    return { searchResults }
+    const { averageRating, reviews } = await getInitialRatingsAndReviews(params.productId)
+    return { searchResults, averageRating, reviews }
 }
 
 export default function NewProduct () {
-    const { searchResults } = useLoaderData()
-    const product = <ShoppingProduct {...searchResults} addToCart={addToCart} view="fullSize"/>
+    const { searchResults, averageRating, reviews } = useLoaderData()
+    const isLoggedIn = React.useContext(LoginContext)
+    const product = <ShoppingProduct {...searchResults} averageRating={averageRating} addToCart={addToCart} view="fullSize"/>
 
     return (
         <>
             {product ?? <h1>no product</h1>}
+            {isLoggedIn && <ReviewBox initialReviews={reviews} productId = {searchResults.details.id} />}
         </>
     )
 }
