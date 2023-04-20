eedbc939c2a870d81b5575fc55cfa5c9a039e0df
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-04-20 15:08:16 -0400

    redo homepage
    Have category buttons for search navigation, or "BEST SELLERS" for instant jump to a popular product. Atm they are hardcoded, but not so difficult to pull top5 most popular from backend.

--- src/Routes/NewHomePage.jsx
+++ src/Routes/NewHomePage.jsx
@@ -1,8 +1,68 @@
 import React from "react"
-import upwardArrow from "../krita-vertical-arrow.png"
+import { Link } from "react-router-dom"
+import shirt from "../t-shirt-preview.png"
 
 export default function NewHomePage () {
     return (
-        <div className="homepageArrowBackground" style={{ backgroundImage: `url('${upwardArrow}')` }}></div>
+        <div className="homePageContainer">
+            <div className="categoryButtonContainer">
+                <Link className="categoryButton" to="/s/Shirt">Shirts</Link>
+                <Link className="categoryButton" to="/s/Short">Shorts</Link>
+                <Link className="categoryButton" to="/s/Shirt">Shirts</Link>
+            </div>
+            <div className="bestsellerContainer">
+                <h1 className="bestsellerContainer--title">Best Sellers</h1>
+                <div className="popularItemsContainer">
+                    <Link to={"/p/Generic+T+Shirt/id/0"} className="popularItem">
+                        <div className="popularItem--image" style={{ backgroundImage: `url(${shirt})` }}></div>
+                        <div className="popularItem--details">
+                            <h1>Generic T Shirt</h1>
+                            <p>No.1 item purchased</p>
+                            <p>⭐⭐⭐⭐⭐</p>
+                        </div>
+                    </Link>
+                    <Link to={"/p/Generic+T+Shirt/id/0"} className="popularItem">
+                        <div className="popularItem--image" style={{ backgroundImage: `url(${shirt})` }}></div>
+                        <div className="popularItem--details">
+                            <h1>Specific T Shirt</h1>
+                            <p>No.2 item purchased</p>
+                            <p>⭐⭐⭐⭐</p>
+                        </div>
+                    </Link>
+                    <Link to={"/p/Generic+T+Shirt/id/0"} className="popularItem">
+                        <div className="popularItem--image" style={{ backgroundImage: `url(${shirt})` }}></div>
+                        <div className="popularItem--details">
+                            <h1>T Shirt 3</h1>
+                            <p>No.3 item purchased</p>
+                            <p>⭐⭐⭐</p>
+                        </div>
+                    </Link>
+                    <Link to={"/p/Generic+T+Shirt/id/0"} className="popularItem">
+                        <div className="popularItem--image" style={{ backgroundImage: `url(${shirt})` }}></div>
+                        <div className="popularItem--details">
+                            <h1>T Shirt 3</h1>
+                            <p>No.3 item purchased</p>
+                            <p>⭐⭐⭐</p>
+                        </div>
+                    </Link>
+                    <Link to={"/p/Generic+T+Shirt/id/0"} className="popularItem">
+                        <div className="popularItem--image" style={{ backgroundImage: `url(${shirt})` }}></div>
+                        <div className="popularItem--details">
+                            <h1>T Shirt 3</h1>
+                            <p>No.3 item purchased</p>
+                            <p>⭐⭐⭐</p>
+                        </div>
+                    </Link>
+                    <Link to={"/p/Generic+T+Shirt/id/0"} className="popularItem">
+                        <div className="popularItem--image" style={{ backgroundImage: `url(${shirt})` }}></div>
+                        <div className="popularItem--details">
+                            <h1>T Shirt 3</h1>
+                            <p>No.3 item purchased</p>
+                            <p>⭐⭐⭐</p>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
