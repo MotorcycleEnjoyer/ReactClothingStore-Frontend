f6cc43554355495357b686f5759988448d65b5c1
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-04-24 15:49:29 -0400

    add async function to update data

--- src/Routes/NewProduct.jsx
+++ src/Routes/NewProduct.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useLoaderData } from "react-router-dom"
-import { getProduct, addToCart, getInitialRatingsAndReviews } from "../API/apiCalls"
+import { getProduct, addToCart, getInitialRatingsAndReviews, addProductReview } from "../API/apiCalls"
 import { LoginContext } from "../Contexts/ShoppingContext"
 import ShoppingProduct from "../Components/ShoppingProduct/ShoppingProduct"
 import ReviewBox from "../Components/SmallComponents/ReviewBox/ReviewBox"
@@ -14,13 +14,20 @@ export async function loader ({ params }) {
 
 export default function NewProduct () {
     const { searchResults, averageRating, reviews } = useLoaderData()
+    const [override, setOverride] = React.useState(null)
     const isLoggedIn = React.useContext(LoginContext)
-    const product = <ShoppingProduct {...searchResults} averageRating={averageRating} addToCart={addToCart} view="fullSize"/>
+    const product = <ShoppingProduct {...searchResults} averageRating={override?.averageRating || averageRating} addToCart={addToCart} view="fullSize"/>
+
+    async function getNewData (payload) {
+        const bigData = await addProductReview(searchResults.details.id, payload)
+        const newPayload = { reviews: bigData.reviews, averageRating: bigData.averageRating }
+        setOverride(() => newPayload)
+    }
 
     return (
         <>
             {product ?? <h1>no product</h1>}
-            <ReviewBox initialReviews={reviews} loggedIn={isLoggedIn} productId = {searchResults.details.id} />
+            <ReviewBox initialReviews={override?.reviews || reviews} getNewData={getNewData} loggedIn={isLoggedIn} productId = {searchResults.details.id} />
         </>
     )
 }
