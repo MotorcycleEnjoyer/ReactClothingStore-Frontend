90bbb8b380a9033de23b280f30065c089b91a7bc
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-05-01 12:16:16 -0400

    use react router Form and action.
    also use replace prop, so you can't navigate back to the login page.

--- src/Routes/NewLogin.jsx
+++ src/Routes/NewLogin.jsx
@@ -1,40 +1,43 @@
 import React from "react"
 import { login } from "../API/apiCalls"
 import { LoginContext } from "../Contexts/ShoppingContext"
+import { Form, redirect } from "react-router-dom"
+
+export async function action ({ request }) {
+    const formData = await request.formData()
+    const credentials = Object.fromEntries(formData)
+    const result = await login(credentials)
+    if (result?.status === 200) {
+        return redirect("/")
+    }
+    if (result?.status === 401) {
+        document.querySelector(".serverResponse").innerText = result.data
+    }
+}
 
 export default function Login () {
-    async function handleSubmit (e) {
-        e.preventDefault()
-        const rawFormData = new FormData(e.target)
-        const credentials = Object.fromEntries(rawFormData)
-        const result = await login(credentials)
-        if (result?.status === 401) {
-            document.querySelector(".serverResponse").innerText = result.data
-        }
+    const isLoggedIn = React.useContext(LoginContext)
+    if (isLoggedIn) {
+        window.location.href = "/"
     }
 
-    const isLoggedIn = React.useContext(LoginContext)
-    React.useEffect(() => {
-        if (isLoggedIn) {
-            window.location.href = "/"
-        }
-    }, [])
-
     return (
-        <>
-            { !isLoggedIn &&
-                <div className="auth">
-                    <h1>Please enter your credentials</h1>
-                    <form onSubmit={handleSubmit}>
-                        <div className="credentialBox">
-                            <input type="text" id="username" name="username" placeholder='Username' required maxLength="30" autoFocus></input>
-                            <input type="password" id="password" name="password" placeholder='Password' required maxLength="30"></input>
-                        </div>
-                        <button>Submit</button>
-                    </form>
-                    <div className="serverResponse"></div>
-                </div>
-            }
+        <> {!isLoggedIn &&
+            <div className="auth">
+                <h1>Please enter your credentials</h1>
+                <Form
+                    method="post"
+                    replace
+                >
+                    <div className="credentialBox">
+                        <input type="text" id="username" name="username" placeholder='Username' required maxLength="30" autoFocus></input>
+                        <input type="password" id="password" name="password" placeholder='Password' required maxLength="30"></input>
+                    </div>
+                    <button>Submit</button>
+                </Form>
+                <div className="serverResponse"></div>
+            </div>
+        }
         </>
     )
 }
