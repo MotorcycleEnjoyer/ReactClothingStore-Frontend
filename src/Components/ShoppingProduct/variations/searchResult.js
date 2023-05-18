87aed63d4441f90dfec0bde67a7b67bf7f3a3408
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-05-18 13:27:05 -0400

    remove unused prop

--- src/Components/ShoppingProduct/variations/searchResult.js
+++ src/Components/ShoppingProduct/variations/searchResult.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-export default function SearchResult ({ details, name, redirectToProductView }) {
+export default function SearchResult ({ details, redirectToProductView }) {
     return (
         <div className="shoppingProduct--searchResult--mid" onClick={() => redirectToProductView(details.id)}>
             <Link className={`Link${details.id}`} style={{ display: "none" }} to={`/p/${details.name.split(" ").join("+")}/id/${details.id}`}></Link>
