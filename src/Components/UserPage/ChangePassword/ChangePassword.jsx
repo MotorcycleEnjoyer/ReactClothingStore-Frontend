86a937f3fd7ede45c8158cbf882224848ac3af1d
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-09-27 11:45:38 -0400

    eslint

--- src/Components/UserPage/ChangePassword/ChangePassword.jsx
+++ src/Components/UserPage/ChangePassword/ChangePassword.jsx
@@ -1,59 +1,80 @@
-import React from "react"
-import { Form } from "react-router-dom"
-import { changePassword } from "../../../API/apiCalls"
+import React from "react";
+import { Form } from "react-router-dom";
+import { changePassword } from "../../../API/apiCalls";
 
-export async function action ({ request }) {
-    const formData = await request.formData()
-    const credentials = Object.fromEntries(formData)
-    const response = await changePassword(credentials)
+export async function action({ request }) {
+    const formData = await request.formData();
+    const credentials = Object.fromEntries(formData);
+    const response = await changePassword(credentials);
     if (response === 200) {
-        const conf = confirm("Password change success! Click OK to go to homepage.")
+        const conf = window.confirm(
+            "Password change success! Click OK to go to homepage."
+        );
         if (conf) {
-            window.location = "/"
+            window.location = "/";
         }
     } else {
-        alert("Password change failure!")
+        alert("Password change failure!");
     }
 }
 
-export default function ChangePassword () {
-    const [password, setPassword] = React.useState("")
-    const [confirmPassword, setConfirmPassword] = React.useState("")
+export default function ChangePassword() {
+    const [password, setPassword] = React.useState("");
+    const [confirmPassword, setConfirmPassword] = React.useState("");
 
-    function handleConfirmPwChange (e) {
-        setConfirmPassword(e.target.value)
+    function handleConfirmPwChange(e) {
+        setConfirmPassword(e.target.value);
     }
 
-    function handlePwChange (e) {
-        setPassword(e.target.value)
+    function handlePwChange(e) {
+        setPassword(e.target.value);
     }
 
     const passwordStyle = {
         pw: {
-            border: password.length >= 8 ? "5px solid green" : "5px solid black"
+            border:
+                password.length >= 8 ? "5px solid green" : "5px solid black",
         },
         cpw: {
-            border: confirmPassword.length >= 8 && confirmPassword === password ? "5px solid green" : "5px solid black"
-        }
-    }
+            border:
+                confirmPassword.length >= 8 && confirmPassword === password
+                    ? "5px solid green"
+                    : "5px solid black",
+        },
+    };
 
     return (
         <div className="auth">
             <h1>Change Password</h1>
-            <Form
-                method="post"
-                className="changePassword"
-            >
+            <Form method="post" className="changePassword">
                 <div className="credentialBox">
-                    <input style={ passwordStyle.pw } type="password" id="password" onChange={handlePwChange} name="password" placeholder="Password" value={password} required maxLength="30"></input>
-                    <input style={ passwordStyle.cpw } type="password" id="confirmPassword" onChange={handleConfirmPwChange} name="confirmPassword" value={confirmPassword} placeholder="Confirm Password" required maxLength="30"></input>
+                    <input
+                        style={passwordStyle.pw}
+                        type="password"
+                        id="password"
+                        onChange={handlePwChange}
+                        name="password"
+                        placeholder="Password"
+                        value={password}
+                        required
+                        maxLength="30"
+                    ></input>
+                    <input
+                        style={passwordStyle.cpw}
+                        type="password"
+                        id="confirmPassword"
+                        onChange={handleConfirmPwChange}
+                        name="confirmPassword"
+                        value={confirmPassword}
+                        placeholder="Confirm Password"
+                        required
+                        maxLength="30"
+                    ></input>
                 </div>
 
-                {
-                    (password === confirmPassword && confirmPassword.length >= 8) &&
-                    <button>Submit</button>
-                }
+                {password === confirmPassword &&
+                    confirmPassword.length >= 8 && <button>Submit</button>}
             </Form>
         </div>
-    )
+    );
 }
--- src/Routes/NewCart.jsx
+++ src/Routes/NewCart.jsx
@@ -1,108 +1,153 @@
-import React, { useContext } from "react"
-import ShoppingProduct from "../Components/ShoppingProduct/ShoppingProduct"
-import { ShoppingCartContext, ShoppingCartDispatchContext } from "../Contexts/ShoppingContext"
-import { Link } from "react-router-dom"
+import React, { useContext } from "react";
+import ShoppingProduct from "../Components/ShoppingProduct/ShoppingProduct";
+import {
+    ShoppingCartContext,
+    ShoppingCartDispatchContext,
+} from "../Contexts/ShoppingContext";
+import { Link } from "react-router-dom";
 // import { checkoutWithStripe } from "../API/apiCalls"
 
-export default function NewCart () {
-    const shoppingCart = useContext(ShoppingCartContext)
-    const dispatch = useContext(ShoppingCartDispatchContext)
+export default function NewCart() {
+    const shoppingCart = useContext(ShoppingCartContext);
+    const dispatch = useContext(ShoppingCartDispatchContext);
 
-    const iniVal = 0
-    const totalCost = shoppingCart
-        .reduce(
-            (accumulator, currentVal) => accumulator + (currentVal.details.price * currentVal.amount),
-            iniVal)
+    const iniVal = 0;
+    const totalCost = shoppingCart.reduce(
+        (accumulator, currentVal) =>
+            accumulator + currentVal.details.price * currentVal.amount,
+        iniVal
+    );
 
-    const [activeCartItem, setActiveCartItem] = React.useState(null)
+    const [activeCartItem, setActiveCartItem] = React.useState(null);
     React.useEffect(() => {
         if (activeCartItem) {
-            showCartModal()
+            showCartModal();
         }
-    }, [activeCartItem])
+    }, [activeCartItem]);
 
-    function deleteCartItem (index) {
+    function deleteCartItem(index) {
         dispatch({
             type: "deleteCartItem",
-            properties: index
-        })
+            properties: index,
+        });
     }
 
-    function changeCartItem (objectHeaders) {
+    function changeCartItem(objectHeaders) {
         dispatch({
             type: "editCartItem",
-            properties: objectHeaders
-        })
+            properties: objectHeaders,
+        });
     }
 
-    function clearCart () {
-        const yesIWantToDelete = confirm("Are you sure you want to clear your cart?")
+    function clearCart() {
+        const yesIWantToDelete = window.confirm(
+            "Are you sure you want to clear your cart?"
+        );
         if (yesIWantToDelete) {
             dispatch({
-                type: "clearCart"
-            })
+                type: "clearCart",
+            });
         }
     }
 
-    function showCartModal (e) {
-        const modalStyle = document.querySelector(".cartItem--modal").style
-        modalStyle.display = "block"
+    function showCartModal(e) {
+        const modalStyle = document.querySelector(".cartItem--modal").style;
+        modalStyle.display = "block";
     }
 
-    function checkClickClassName (e) {
+    function checkClickClassName(e) {
         if (e.target.className === "cartItem--modal") {
-            hideCartModal()
+            hideCartModal();
         }
     }
 
-    function hideCartModal () {
-        const modalStyle = document.querySelector(".cartItem--modal")
-        modalStyle.style.display = "none"
-        setActiveCartItem(() => null)
+    function hideCartModal() {
+        const modalStyle = document.querySelector(".cartItem--modal");
+        modalStyle.style.display = "none";
+        setActiveCartItem(() => null);
     }
 
-    function activateCartModal (propsFromCartItem) {
-        const itemToAppend = <ShoppingProduct {...propsFromCartItem} modal={true} editCartItem={changeCartItem} hideCartModal={hideCartModal} toggleCartModal={activateCartModal} view="fullSize"/>
-        setActiveCartItem(itemToAppend)
+    function activateCartModal(propsFromCartItem) {
+        const itemToAppend = (
+            <ShoppingProduct
+                {...propsFromCartItem}
+                modal={true}
+                editCartItem={changeCartItem}
+                hideCartModal={hideCartModal}
+                toggleCartModal={activateCartModal}
+                view="fullSize"
+            />
+        );
+        setActiveCartItem(itemToAppend);
     }
 
-    function handleFormSubmit () {
+    function handleFormSubmit() {
         dispatch({
             type: "submitOrder",
-            properties: shoppingCart
-        })
+            properties: shoppingCart,
+        });
     }
 
-    const cartAsHTML = shoppingCart?.map((item, index) => <ShoppingProduct key={index} index={index} removeFromCart={() => deleteCartItem(index)} {...item} toggleCartModal={activateCartModal} view={"cart"} />)
+    const cartAsHTML = shoppingCart?.map((item, index) => (
+        <ShoppingProduct
+            key={index}
+            index={index}
+            removeFromCart={() => deleteCartItem(index)}
+            {...item}
+            toggleCartModal={activateCartModal}
+            view={"cart"}
+        />
+    ));
 
     return (
         <>
-            { shoppingCart?.length > 0 &&
-            <>
-                <div className="cartItem--modal" style={{ display: "none" }} onClick={checkClickClassName}>
-                    <div className="cartItem--modal--content" >
-                        <button onClick={hideCartModal} style={{ padding: "20px", marginRight: "0px" }}>Close Edit Menu</button>
-                        <h1 className="modalContentHeader">Edit Item</h1>
-                        {activeCartItem}
+            {shoppingCart?.length > 0 && (
+                <>
+                    <div
+                        className="cartItem--modal"
+                        style={{ display: "none" }}
+                        onClick={checkClickClassName}
+                    >
+                        <div className="cartItem--modal--content">
+                            <button
+                                onClick={hideCartModal}
+                                style={{ padding: "20px", marginRight: "0px" }}
+                            >
+                                Close Edit Menu
+                            </button>
+                            <h1 className="modalContentHeader">Edit Item</h1>
+                            {activeCartItem}
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <h1 style={{ color: "black" }}>TOTAL: {totalCost.toFixed(2) }<span></span></h1>
-                    <button style={{ padding: "1rem" }} onClick={handleFormSubmit}>Checkout</button>
-                    {/* <button style={{ padding: "1rem" }} onClick={() => { checkoutWithStripe(shoppingCart) }}>Checkout</button> */}
-                </div>
-                <div className="cartItems" >
-                    {cartAsHTML}
-                </div>
-                <button className="clearCartButton" onClick={clearCart}>CLEAR CART</button>
-            </>
-            }
-            { shoppingCart?.length === 0 &&
-            <>
-                <h1 >Your shopping cart is empty</h1>
-                <Link to="/"><button style={{ padding: "1rem" }}>Back to home</button></Link>
-            </>
-            }
+                    <div>
+                        <h1 style={{ color: "black" }}>
+                            TOTAL: {totalCost.toFixed(2)}
+                            <span></span>
+                        </h1>
+                        <button
+                            style={{ padding: "1rem" }}
+                            onClick={handleFormSubmit}
+                        >
+                            Checkout
+                        </button>
+                        {/* <button style={{ padding: "1rem" }} onClick={() => { checkoutWithStripe(shoppingCart) }}>Checkout</button> */}
+                    </div>
+                    <div className="cartItems">{cartAsHTML}</div>
+                    <button className="clearCartButton" onClick={clearCart}>
+                        CLEAR CART
+                    </button>
+                </>
+            )}
+            {shoppingCart?.length === 0 && (
+                <>
+                    <h1>Your shopping cart is empty</h1>
+                    <Link to="/">
+                        <button style={{ padding: "1rem" }}>
+                            Back to home
+                        </button>
+                    </Link>
+                </>
+            )}
         </>
-    )
+    );
 }
